0b1167a13961029b2d55c809c50a8d6ae53dc759
CZvino <ninja_zfan@126.com>
2016-03-27 17:25:33 +0800

    计算类和数类实现-version 1
    初步实现计算类和数类，支持负数、小数。除算式第一个数字外，其余的负数必须带括号。（-3+5）这种除外。两个类分别存与Calculator.cs和Number.cs。其中Calculator实现为静态类。
    
    数类实现存储运算的结果（分子/分母的形式），提供四种构造函数，重载了+-*/符号，实现了三种toString函数，分别为输出分数形式、小数形式、带精度控制的小数形式。
    
    计算类主要是两个函数，中缀转后缀和后缀计算。其中中缀转后缀传入变量为一个串，可对该串进行转换，并判断算式是否合法（输出bool），同时判断算式的结果是否需要小数形式的输出（算式是否含小数点，输出）。返回值是一个串（表达式合法时，返回带空格形式的后缀表达式，不合法时返回对应的错误信息）。目前想到的错误主要有：括号不匹配、含非法字符、两符号相邻（+-等非法情况）、小数点不位于数字中间、小数点过多。
    
    后缀计算函数输入为一个串，输出也为一个串。输入还需要传入是否有小数、表达式是否合法、是否有精确度、以及精确度位数的信息。然后就直接计算就好了。最后的结果根据传入的后几个参数决定其输出形式。

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        private static bool isOpt(char x)
+        {
+            return (x == '+' || x == '-' || x == '*' || x == '/' || x == '(' || x == ')');
+        }
+
+        private static bool isNum(char x)
+        {
+            return (x >= '0' && x <= '9');
+        }
+
+        private static int priority(char x)
+        {
+            if (x == '-' || x == '+')
+                return 1;
+            else if (x == '*' || x == '/')
+                return 2;
+            else if (x == '(')
+                return 0;
+            else
+                return -1;
+        }
+
+        public static String nifixToPost(String nifixString, out bool hasDemical, out bool error)
+        {
+            Stack<char> s = new Stack<char>();
+            bool hasNum = false;
+            bool hasDemicalDuringScan = false;
+            bool minusFlag = false;
+            long demicalDigitsNum = 1;
+            long tmpNum = 0;
+
+            String res = "";
+            hasDemical = false;
+            error = false;
+
+            for (int i = 0; i < nifixString.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if (nifixString[i] == '-')
+                    {
+                        minusFlag = true;
+                        i++;
+                    }
+                    else if (nifixString[i] == '(') ;
+                    else if (isOpt(nifixString[i]))
+                    {
+                        error = true;
+                        return "ERROR-Expression begin with wrong character!";
+                    }
+                }
+                if (isNum(nifixString[i]))
+                {
+                    hasNum = true;
+                    tmpNum = tmpNum * 10 + (nifixString[i] - '0');
+                    if (hasDemicalDuringScan)
+                        demicalDigitsNum *= 10;
+                }
+                else if (isOpt(nifixString[i]))
+                {
+                    if (i > 0)
+                    {
+                        if (nifixString[i] == '-' && nifixString[i - 1] == '(') ;
+                        else if (nifixString[i] == '(' && isOpt(nifixString[i - 1]) && nifixString[i - 1] != ')') ;
+                        else if (nifixString[i - 1] == ')') ;
+                        else if (isNum(nifixString[i - 1])) ;
+                        else
+                        {
+                            error = true;
+                            return "ERROR-Two operators next to each other illegally!";
+                        }
+                    }
+                    if (hasNum)
+                    {
+                        hasNum = false;
+                        //Console.WriteLine("minusFlag = {0}, tmpNum = {1}, hasDemicalDuringScan = {2}, {3}", minusFlag, tmpNum, hasDemicalDuringScan, tmpNum+'0');
+                        if (minusFlag)
+                        {
+                            tmpNum = -tmpNum;
+                            minusFlag = false;
+                        }
+                        res += tmpNum;
+                        res += ' ';
+                        tmpNum = 0;
+                        if (hasDemicalDuringScan)
+                        {
+                            if (demicalDigitsNum == 1)
+                            {
+                                error = true;
+                                return "ERROR-Digits absent after radix point!-1";
+                            }
+                            hasDemical = true;
+                            res += demicalDigitsNum;
+                            res += ' ';
+                            demicalDigitsNum = 1;
+                            hasDemicalDuringScan = false;
+                            res += "/ ";
+                        }
+                    }
+                    if (nifixString[i] == '(')
+                    {
+                        s.Push(nifixString[i]);
+                        if (i + 1 < nifixString.Length && nifixString[i + 1] == '-')
+                        {
+                            minusFlag = true;
+                            i++;
+                        }
+                    }
+                    else if (nifixString[i] == ')')
+                    {
+                        if (s.Count == 0)
+                        {
+                            error = true;
+                            return "ERROR-Parentheses unmatched!-1";
+                        }
+                        char tmpOperator;
+                        while (s.Count > 0 && (tmpOperator = s.Pop()) != '(')
+                        {
+                            res += tmpOperator;
+                            res += ' ';
+                            if (s.Count == 0)
+                            {
+                                error = true;
+                                return "ERROR-Parentheses unmatched!-2";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        char tmpTop = ' ';
+                        bool flag = false;
+                        while (true)
+                        {
+                            if (s.Count == 0)
+                                break;
+                            tmpTop = s.Pop();
+                            if (priority(nifixString[i]) > priority(tmpTop))
+                            {
+                                flag = true;
+                                break;
+                            }
+                            res += tmpTop;
+                            res += ' ';
+                        }
+                        if (flag)
+                            s.Push(tmpTop);
+                        s.Push(nifixString[i]);
+                    }
+                }
+                else if (nifixString[i] == '.')
+                {
+                    if (hasDemicalDuringScan || !hasNum)
+                    {
+                        error = true;
+                        return "ERROR-Too more radix point or has no digits before radix point!";
+                    }
+                    else
+                        hasDemicalDuringScan = true;
+                }
+                else
+                {
+                    error = true;
+                    return "ERROR-Illegal character!";
+                }
+            }
+            if (hasNum)
+            {
+                hasNum = false;
+                if (minusFlag)
+                {
+                    tmpNum = -tmpNum;
+                    minusFlag = false;
+                }
+                res += tmpNum;
+                res += ' ';
+                tmpNum = 0;
+                if (hasDemicalDuringScan)
+                {
+                    if (demicalDigitsNum == 1)
+                    {
+                        error = true;
+                        return "ERROR-Digits absent after radix point!-1";
+                    }
+                    hasDemical = true;
+                    res += demicalDigitsNum;
+                    res += ' ';
+                    demicalDigitsNum = 1;
+                    hasDemicalDuringScan = false;
+                    res += "/ ";
+                }
+            }
+            while (s.Count > 0)
+            {
+                char tmpPop = s.Pop();
+                if (tmpPop == '(')
+                {
+                    error = true;
+                    return "ERROR-Parentheses unmatched!-3";
+                }
+                res += tmpPop;
+                res += ' ';
+            }
+            return res.Substring(0, res.Length - 1);
+        }
+
+        public static String postCalculate(String postString, bool hasDemical, bool error, bool hasPricision, int precision)
+        {
+            Stack<Number> s = new Stack<Number>();
+            if (postString.Length >= 5 && postString.Substring(0, 5).Equals("ERROR"))
+                return postString;
+            if (error)
+                return "ERROR";
+
+            String[] sArray = postString.Split(' ');
+            foreach (String i in sArray)
+            {
+                if (i.Length == 1 && isOpt(i[0]))
+                {
+                    Number b = new Number(s.Pop());
+                    Number a = new Number(s.Pop());
+                    switch (i[0])
+                    {
+                        case '+': s.Push(new Number(a + b)); break;
+                        case '-': s.Push(new Number(a - b)); break;
+                        case '*': s.Push(new Number(a * b)); break;
+                        case '/': s.Push(new Number(a / b)); break;
+                        default: return "ERROR-Illegal character";
+                    }
+                }
+                else
+                    s.Push(new Number(Convert.ToInt64(i)));
+            }
+            if (s.Count > 1)
+                return "ERROR";
+            if (hasPricision)
+                return s.Pop().toString(precision);
+            else if (hasDemical)
+                return s.Pop().toString(hasDemical);
+            else
+                return s.Pop().toString();
+        }
+    }
+}
