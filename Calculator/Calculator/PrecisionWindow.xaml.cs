825cff87aefc9fb4d3f0ace8c226ef925859482a
syc728 <bjxx5555@126.com>
2016-04-03 18:23:38 +0800

    在UI增加设置的工具栏，提供精度控制的入口

---
+++ Calculator/Calculator/PrecisionWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// PrecisionWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class PrecisionWindow : Window
+    {
+        public PrecisionWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Precision_Control(object sender, RoutedEventArgs e)
+        {
+            MainWindow.precision = Convert.ToInt32(precisionControl.Text);
+            this.Close();
+        }
+
+        private void Window_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!isNumberic(e.Text))
+            {
+                e.Handled = true;
+            }
+            else
+                e.Handled = false;
+        }
+
+        private static bool isNumberic(string _string)
+        {
+            if (string.IsNullOrEmpty(_string))
+                return false;
+            foreach (char c in _string)
+            {
+                if (!char.IsDigit(c))
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
